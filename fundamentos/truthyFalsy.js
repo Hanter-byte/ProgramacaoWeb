ad99663d7d03e6a18f1c11ebb8f98f17d6d9f69e
João Pedro <jp663687@gmail.com>
2022-04-27 11:51:51 -0300

    03

--- fundamentos/truthyFalsy.js
+++ fundamentos/truthyFalsy.js
@@ -3,5 +3,25 @@
 // 0 => false
 // 1 => true
 
-console.log(0 == false)
-console.log("" == false)
+let carro;
+let moto = null;
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+console.log();
+
+console.log(carro);
+console.log(moto);
+console.log();
+
+//undefined => vazio ou nada
+// null
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof carro);
+console.log(typeof moto);
+console.log(typeof numero);
+console.log(typeof texto)
